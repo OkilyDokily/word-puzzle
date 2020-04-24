2028cb02ca99aa3b02a8b5eddc4f7913f0d9c8fd
me <me@me.me.com>
2020-04-24 15:50:10 -0700

    fill out the README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# _Word Puzzle_
+
+#### _This demonstrates knowledge of JavaScript._
+
+#### By _**Matthew LeDoux**_
+
+## Description
+
+_A page which demonstrates beginner JavaScript with for loops ._
+_takes a sentence and replaces the vowels with a '-'._
+
+## Setup/Installation Requirements
+
+Clone the repo and open the .html file in a browser.
+
+## Known Bugs
+
+_I don't think there are any bugs but please tell me if you see any._
+
+## Support and contact details
+
+_Open a Github issue if you see a problem or have questions_
+
+## Technologies Used
+
+_This is an html, css, Bootstrap, jQuery, and JavaScript thing._
+
+### License
+
+*Feel free to use this code as you please*
+
+Copyright (c) 2020 **_Matthew LeDoux_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,34 @@
 var vowels = ["a","e","i","o","u"];
+
+
 function makePuzzle(sentence){
-  for(i =0, i < sentence.length; i++){
-    if (vowels.includes(sentence[i])){
-      sentence[i] = "-";
+  var newSentence = "";
+  for(i = 0; i < sentence.length; i++){
+    if (vowels.includes(sentence.toLowerCase()[i])){
+      newSentence += "-"
+    }
+    else{
+      newSentence += sentence[i];
     }
   }
-  return sentence;
+  return newSentence;
 }
 
 $(document).ready(function(){
  $("form").submit(function(e){
+    hideForm();
     e.preventDefault();
     var sentence = $("#sentence").val();
     var result = makePuzzle(sentence);
     $("p").text(result);
+    
+    function hideForm(){
+      $("form").hide();
+      $("#showform").show();
+    }
+ });
+
+ $("#showform").click(function(){
+    $("form").show();
  });
 })
